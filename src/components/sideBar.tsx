ffecd93c3bfff3643554c8aea7cb7b83f90357f8
0rbita1 <E1384355@U.NUS.EDU>
2026-03-08 09:57:11 +0800

    Implement rendering of filenames in sidebar

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { listNotes, type NoteMetadata } from "../services/fileService";
 import "../styles/sideBar.css";
 
 interface SideBarProps {
@@ -5,9 +7,27 @@ interface SideBarProps {
 }
 
 function SideBar({ isOpen }: SideBarProps) {
+  const [noteList, setNoteList] = useState<NoteMetadata[]>([]);
+
+  useEffect(() => {
+    if (isOpen) {
+      listNotes().then(setNoteList);
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
-  return <div className="sidebar" />;
+  return (
+    <div className="sidebar">
+      <ul className="sidebar-note-list">
+        {noteList.map((note) => (
+          <li key={note.filename} className="sidebar-note-item">
+            {note.filename}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar;
